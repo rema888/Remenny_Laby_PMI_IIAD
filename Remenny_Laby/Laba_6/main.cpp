2ef0e577890004d5ad513b36dc16b8dbcd58d40e
rema888 <aremenny@gmail.com>
2024-10-26 16:08:24 +0200

    Add: 6 лабораторная работа - тест (задание 1)

--- Remenny_Laby/Laba_6/main.cpp
+++ Remenny_Laby/Laba_6/main.cpp
@@ -5,19 +5,20 @@ using namespace std;
 int main()
 {
     // Задание 1
+
     int mas[10000];
-    int lastDigit[10000];
     bool ravenstvo = false;
     int n;
     std::cout << "Введите длину массива: "; std::cin >> n;
     std::cout << "Введите элементы массива: ";
    
+    // Ввод массива
     for(int i=0;i<n;i++)
     {
         std::cin >> mas[i];
-        lastDigit[i] = (mas[i] % 10);
     }
 
+    // Проверка на наличие одинаковых чисел в массиве 
     for(int i=0;i<n;i++)
         for(int j=i+1;j<n;j++)
             if (mas[i] == mas[j])
@@ -25,12 +26,13 @@ int main()
                 ravenstvo = true;
                 break;
             }
-    //swap sort
+
+    // Сортировка обменом (Swap sort)
     if (ravenstvo)
     {
         for(int i=0;i<n;i++)
             for(int j=i+1;j<n;j++)
-                if (lastDigit[i] < lastDigit[j])
+                if ((mas[i] % 10) < (mas[j] % 10))
                 {
                     int tmp = mas[i];
                     mas[i] = mas[j];
