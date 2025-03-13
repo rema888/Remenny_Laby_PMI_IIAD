8d934e4570f156e03cc7c60516248cf8373d0c71
rema888 <aremenny@gmail.com>
2025-03-13 20:24:50 +0200

    Add: 12 лабораторная работа

--- Remenny_Laby/Laba_12/main.cpp
+++ Remenny_Laby/Laba_12/main.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
 #include <fstream>
 
-unsigned long long factorial(int n) 
+unsigned long long factorial(int n)
 {
-    if (n <= 1) 
+    if (n <= 1)
         return 1;    
     return n * factorial(n - 1);
 }
 
-void Search(int i, int j, int step, int n, int m, char field[100][100], int table[100][100])
+void Search(int i, int j, int step, int n, int m, char field[100][100], int table[100][100]) 
 {
-    if (step >= table[i][j])
+    if(step >= table[i][j])
         return;
 
-    table[i][j] = step;    
+    table[i][j] = step;
 
     if(field[i][j] == 'E')
         return;
-    // Все возможные ходы
-    if (i + 1 < n && field[i+1][j] != '#')    
+
+    if(i + 1 < n && field[i+1][j] != '#')
         Search(i+1,j,step+1,n,m,field,table);
-    if (i - 1 >= 0 && field[i-1][j] != '#')  
+    if(i - 1 >= 0 && field[i-1][j] != '#')
         Search(i-1,j,step+1,n,m,field,table);
-    if (j + 1 < m && field[i][j+1] != '#')  
+    if(j + 1 < m && field[i][j + 1] != '#')
         Search(i,j+1,step+1,n,m,field,table);
-    if (j - 1 >= 0 && field[i][j-1] != '#')      
+    if(j - 1 >= 0 && field[i][j - 1] != '#')
         Search(i,j-1,step+1,n,m,field,table);
 }
 
-int main() 
+int main()
 {
     // Задание 1
     int N;
     std::cout << "Введите число N (от 1 до 19 включительно): ";
     std::cin >> N;
-    if (N < 1 || N > 19) 
+    if (N < 1 || N > 19)
     {
         std::cout << "Ошибка: N должно быть в диапазоне от 1 до 19." << std::endl;
     }
     std::cout << "Факториал числа " << N << " равен " << factorial(N) << std::endl;
+
 
     // Задание 3
     std::ifstream in("input.txt");
